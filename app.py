c912814c085f3432b0e344e228e40d603e76f095
Joel <joelfigueiridomolares@gmail.com>
2025-05-15 12:07:33 +0200

    fix:provincias.json bug whith BOM character

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 from flask_cors import CORS
+import os
 app = Flask(__name__)
 CORS(app)
 @app.route("/")
@@ -23,8 +24,12 @@ def get_familias():
 @app.get('/provincias')
 def get_provincias():
     try:
-        with open('api/provincias.json', 'r', encoding='utf-8') as file:
+        file_path = os.path.join(app.root_path, 'api', 'provincias.json')
+        with open('api/provincias.json', 'r', encoding='utf-8-sig') as file:
             provincias = json.load(file)
         return provincias
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        app.logger.error(f"Error leyendo {file_path}: {e}")
+        return {'error': str(e)}, 500
     except FileNotFoundError:
         return {'error': 'No se ha podido leer el archivo'}, 404
