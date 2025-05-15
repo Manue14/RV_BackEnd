936e590b83254eb5a1e98af2f94ceaf1e8ad3b09
Joel <joelfigueiridomolares@gmail.com>
2025-05-15 10:59:24 +0200

    feat:Create endpoints for familias.json and provincias.json

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
+import json
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -9,3 +10,21 @@ def hello_world():
 @app.route('/test')
 def hello():
     return {'test1': 1, 'test2': 2}
+
+@app.get('/familias')
+def get_familias():
+    try:
+        with open('api/familias.json', 'r', encoding='utf-8') as file:
+            familias = json.load(file)
+        return familias
+    except FileNotFoundError:
+        return {'error': 'No se ha podido leer el archivo'}, 404
+
+@app.get('/provincias')
+def get_provincias():
+    try:
+        with open('api/provincias.json', 'r', encoding='utf-8') as file:
+            provincias = json.load(file)
+        return provincias
+    except FileNotFoundError:
+        return {'error': 'No se ha podido leer el archivo'}, 404
