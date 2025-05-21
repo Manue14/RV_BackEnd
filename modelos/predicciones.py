586d8a7390580a3d0429550c2881cf48f96caa1e
yelkov <yelkovq@gmail.com>
2025-05-21 20:19:39 +0200

    añadir predicciones con sarima y con winters y corregir error en el tapb

--- modelos/predicciones.py
+++ modelos/predicciones.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from modelos import predecir_con_lgbm
+from predecir_con_lgbm import predecir_con_lgbm_anual, predecir_con_lgbm_mensual
+from predecir_con_winters import predecir_con_winters
+from predecir_con_sarima import predecir_con_sarima
 import os
 import numpy as np
 
@@ -9,6 +11,7 @@ MODELOS_MENSUAL_DIR = 'modelos/modelos_mensuales'
 def predecir_ventas(producto, tienda=None):
     ventas_df = pd.read_csv('modelos/ventas.csv', dtype={'Tienda': str})
     modelos_df = pd.read_csv('modelos/mejores_modelos.csv', dtype={'Tienda': str})
+    meses = 12
 
     if tienda:
         ventas_filtradas = ventas_df[(ventas_df['Nombre_Producto'] == producto) & (ventas_df['Tienda'] == tienda)]
@@ -24,36 +27,30 @@ def predecir_ventas(producto, tienda=None):
     series.index = pd.to_datetime(series.index).to_period('M').to_timestamp()
 
     modelo_tipo = modelo_info.iloc[0]['Modelo_Ganador']
+    tapb = modelo_info.iloc[0]['TAPB_Min']
     producto_clean = producto.replace(" ", "_")
 
     if modelo_tipo == 'LightGBM':
         modelo_path = os.path.join(MODELOS_ANUAL_DIR, f"modelo_{tienda}_{producto_clean}.pkl")
-        predicciones, tapb = predecir_con_lgbm.predecir_con_lgbm_anual(series, modelo_path)
+        predicciones = predecir_con_lgbm_anual(series, modelo_path)
 
     elif modelo_tipo == 'SARIMA':
-        # predicciones, tapb = predecir_con_sarima(series)
-        # ventas_anuales = predicciones.sum()
-        print(modelo_tipo)
-    
+        predicciones  = predecir_con_sarima(series, meses)
+
     elif modelo_tipo == 'Winters':
-        # predicciones, tapb = predecir_con_winters(series)
-        # ventas_anuales = predicciones.sum()
-        print(modelo_tipo)
+        predicciones = predecir_con_winters(series)
+        
     else:
         raise ValueError("Modelo desconocido")
     
     modelo_mensual_path = os.path.join(MODELOS_MENSUAL_DIR, f"modelo_{tienda}_{producto_clean}.pkl")
-    predicciones_mensuales = predecir_con_lgbm.predecir_con_lgbm_mensual(series, modelo_mensual_path)
+    predicciones_mensuales = predecir_con_lgbm_mensual(series, modelo_mensual_path)
 
     # métricas
     ventas_anuales = predicciones.sum()
     tendencia = 'sobreestimación' if tapb > 0 else 'subestimación'
     confiabilidad = clasificar_confiabilidad(tapb)
     tapb = float(round(tapb, 2)) if not np.isnan(tapb) else None
-
-    # # graficas
-    # fig_path = f'graficas/{producto}_{tienda or "total"}.png'
-    # generar_grafico(series, predicciones, fig_path)
 
     return {
         'producto': producto,
@@ -62,9 +59,7 @@ def predecir_ventas(producto, tienda=None):
         'tapb': tapb,
         'tendencia_estimacion': tendencia,
         'confiabilidad': confiabilidad,
-        'prediccion_mensual': predicciones_mensuales.rename_axis('Periodo').rename(index=lambda x: x.strftime('%Y-%m')).to_dict(),
-        'ventas_anteriores': series.rename_axis('Periodo').rename(index=lambda x: x.strftime('%Y-%m')).to_dict()
-        # 'grafico': fig_path
+        'prediccion_mensual': predicciones_mensuales.to_dict(),
     }
 
 def clasificar_confiabilidad(tapb):
